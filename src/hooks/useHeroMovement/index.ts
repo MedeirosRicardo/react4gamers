44da26bcaab4f551ccbc1088f59425cb066d60c2
Ricardo Medeiros <medeiros.ricardo@outlook.com>
2020-04-17 21:06:50 -0400

    Fixed key bug on useHeroMovement

--- src/hooks/useHeroMovement/index.ts
+++ src/hooks/useHeroMovement/index.ts
@@ -4,10 +4,15 @@ import { EDirection } from '../../settings/constants';
 import { handleNextPosition } from '../../contexts/canvas/helpers';
 
 function useHeroMovement(initialPosition: any) {
+    
     const [positionState, updatePositionState] = useState(initialPosition);
     const [direction, updateDirectoinState] = useState(EDirection.RIGHT);
 
     useEventListener('keydown', (event: any) => {
+        if (event.key.indexOf('Arrow') === -1) {
+            return;
+        }
+        
         const direction = event.key as EDirection;
         const nextPosition = handleNextPosition(direction, positionState);
     
